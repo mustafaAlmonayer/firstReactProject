af15e7ad4f3c82ef6d1f1a40f6a6e600880e8848
mustafaAlmonayer <mustafakamal123435@gmail.com>
2023-03-06 23:17:16 +0300

    commited

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,49 +1,63 @@
 import { useEffect, useState } from 'react';
-import {getTodosForUser} from './api/TodoService';
+import { getTodosForUser, deleteTodo } from './api/TodoService';
 
 function TodoComponent() {
-
 	const nowDate = new Date();
 
-	const [todos, setTodos] = useState([]);
+	const [ todos, setTodos ] = useState([]);
+
+	const [ message, setMessage ] = useState(null);
 
 	useEffect(() => refreshTodos(), []);
 
 	function refreshTodos() {
-		getTodosForUser("mustafa")	
+		getTodosForUser('mustafa')
 			.then((responce) => setTodos(responce.data))
 			.catch((error) => console.log(error))
-			.finally(console.log("clean up"))
+			.finally(console.log('clean up'));
 	}
 
-	
+	function deleteTodos(id) {
+		deleteTodo('mustafa', id)
+			.then(() => {
+				setMessage('todo has been deleted');
+				refreshTodos();
+			})
+			.catch((error) => console.log(error))
+			.finally(console.log('cleanup'));
+	}
+
 	return (
-		<div className='Todo container'>
+		<div className="Todo container">
+			{message && <div className="alert alert-danger">{message}</div>}
 			<table className="table">
 				<thead>
 					<tr>
-						<td>id</td>
-						<td>description</td>
-						<td>completed</td>
-						<td>target date</td>
+						<th>id</th>
+						<th>description</th>
+						<th>completed</th>
+						<th>target date</th>
+						<th>Delete</th>
 					</tr>
 				</thead>
 				<tbody>
-
-					{todos.map(todo => (
-					<tr key={todo.id}>
-						<td>{todo.id}</td>
-						<td>{todo.description}</td>
-						<td>{todo.done.toString()}</td>
-						<td>{todo.targetDate}</td>
-					</tr>
-
+					{todos.map((todo) => (
+						<tr key={todo.id}>
+							<td>{todo.id}</td>
+							<td>{todo.description}</td>
+							<td>{todo.done.toString()}</td>
+							<td>{todo.targetDate}</td>
+							<td>
+								<button className="btn btn-danger" onClick={() => deleteTodos(todo.id)}>
+									Delete
+								</button>
+							</td>
+						</tr>
 					))}
-
 				</tbody>
 			</table>
 		</div>
-	)
+	);
 }
 
 export default TodoComponent;
--- src/components/todo/api/TodoService.jsx
+++ src/components/todo/api/TodoService.jsx
@@ -1,9 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080"
-})
+	baseURL: 'http://localhost:8080'
+});
 
 export function getTodosForUser(username) {
-    return apiClient.get(`/username/${username}/todos`);
+	return apiClient.get(`/username/${username}/todos`);
+}
+
+export function deleteTodo(username, id) {
+	return apiClient.delete(`/username/${username}/todos/${id}`);
 }
